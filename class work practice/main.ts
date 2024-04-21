857fe710e4a18933bdc735dac48007f1d92f8760
M Ismail <musafirbaltistani786@gmail.com>
2024-04-21 00:03:57 -0700

    first commit

---
+++ class work practice/main.ts
@@ -0,0 +1,178 @@
+//unary operators//
+// let a=5;
+// ++a;//6
+// ++a;//7
+// ++a;//8
+// ++a;//9
+// ++a;//10
+// --a;//9
+// --a;//8
+// --a;//7
+// --a;//6
+// --a;//5
+
+// import { log } from "console";
+
+// import { log } from "console";
+
+// import { log } from "console";
+
+// // import { log } from "console";
+
+// // console.log(a);
+// // let i=4;
+// // let num=++i + ++i + 6 + --i -6//5+6+6+5-6=16
+// // console.log(num)
+// let i=10;
+// let num1=++i + ++i + --i +7//11+12+11+7
+// console.log(num1)
+//post increment//
+// let a=6;
+//  a+=3;//6+3
+//  console.log(a)
+//  let i=10;
+//  i +=5;
+//  console.log(i)
+// console.clear();
+// console.log("hello world")
+// let a:number =5;
+// let b: number =2;
+// let c=++a + a++ + --b+ --b + a + ++b + b;//6+6+1+0+7+3=23;
+// console.log(c)
+//logical operator//
+// let budget=1;//10lack
+// let haveGirl=true;
+// budget>=10 && haveGirl==true;
+// console.log(budget>=10 || haveGirl==true);
+// let haveDegree="yes";
+// let isHamzasStd="no";
+// let reslt=haveDegree=="yes"||isHamzasStd=="yes";
+// console.log(reslt)
+
+// let ab = 5;
+// let cd = 8;
+// let relt= ab==7||cd==8;
+// console.log(relt);
+// let num1=10;
+// !(num1>10);
+// console.log(!(num1>10))
+
+// let x = 56
+// let all = !(x < 10);
+// console.log(all);
+//if else statement//
+// let weather="sun";
+// if(weather=="raining"){
+//   console.log("wear a raincoat");
+// }
+// else if(weather=="cloudy"){
+//   console.log("wear a light coat"); 
+// }
+// else{
+//   console.log("wear a sunglasses");
+// }
+
+
+// let clear ="bike"
+// if (clear == "bike") {
+//   console.log("yes")
+// }
+// else {
+//   console.log("no")
+// };
+//turnory operator//
+// let isHungry=false;
+// let snack=isHungry ? "apple": "water";
+// console.log(snack)
+// let isOpen=false;
+// let snack=isOpen ? "buy" :"go another shop";
+// console.log(snack)
+
+// function greet() {
+//     return "hello world";
+
+// }
+// let message=greet();//hello world
+// console.log(message)
+
+// function addition(num1:number,num2:number){
+
+//     return num1+num2;
+// }
+
+// console.log(addition(5,6))
+ 
+// function subtract(nm1:number,nm2:number){
+//     return nm1-nm2;
+// }
+// console.log(subtract(4,7))
+// function multply(nm1:number,nm2:number){
+//     return nm1*nm2;
+// }
+// console.log(multply(4,7))
+// function division(nm1:number,nm2:number):number{
+//     return nm1/nm2;
+// }
+// console.log(division(4,7))
+
+// function greet(message:string){
+//     return message;
+// }
+// console.log(greet("i can i wiill inshallah"));
+//arrow functions//
+
+// let greet1=()=> "hello ali";//dont need to write return when there is only one line
+// console.log()//if there is more than one line of codes then we have to write curly brackets and return
+// //if wwe want to do more work from function mus t add before return
+// function greet2(){
+//     return "hellow khan"
+// }
+// function addition(){
+//     return 2+2;
+// }
+// console.log(addition())
+//  let add=(a:number,b:number,c:string)=> {
+// console.log("when you want to go to gym");
+// console.log("why you should not start gym")
+//   return a-b+c;
+//   console.log("when you want to go to gym");//function will not read this b/c this is after return,
+// console.log("why you should not start gym")
+//  }
+//  console.log(add(6,8,"mine"));
+
+ //variable scopes //
+//  //global variables
+//  let age=23;
+//  if(true){
+//     console.log(age)
+//  }
+//  //local variables
+//  if(true){
+//     let firstName="ismail";
+//     console.log(firstName)
+//  }//remember we could not access the firstName out of {}braces  
+
+// let name="alikhan";
+// if(true){
+//     let name="jawad";
+//     console.log(name)
+// }//always check first int local domain then globally
+
+// let age;//declared variable
+// age=16;//initialized variable
+// console.log(age)
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
---
+++ hello-world/index.ts
@@ -0,0 +1,10 @@
+ 
+//  let greet="hello world";
+//  console.log(greet)
+console.log("hello")
+
+
+//  //assignment no
+// let  usrName=prompt("hello world")
+// //  let message="would you like to learn some python ?";
+// //  console.log(`Eric ${message}`)
